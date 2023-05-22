8786d467682bad7a3a9e3f8b9266480454917a49
Hrishikesh2499 <87982106+Hrishikesh2499@users.noreply.github.com>
2023-05-22 16:50:36 +0530

    Delete api directory

--- api/src/main/java/com/example/quest/repositories/DataManagerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.quest.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.quest.models.DataManager;
-
-public interface DataManagerRepository extends JpaRepository<DataManager,Long> {
-    
-}
